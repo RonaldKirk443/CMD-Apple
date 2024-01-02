f9aaa0419cee0e8edd058fe3c3f29937c45b456e
ddotes <ronaldkirk101pc@gmail.com>
2024-01-02 13:14:48 -0500

    Uses actual video instead of frames

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using FFMpegCore;
+using FFMpegCore.Pipes;
+using System.Diagnostics;
 using System.Drawing;
+using System.Runtime;
 using System.Runtime.CompilerServices;
 
 namespace CMD_Apple
@@ -23,14 +26,19 @@ namespace CMD_Apple
 
             char[] shader = {' ', '.', '\'', 'I', '!', '-', '|', 'X', 'Y', '0', 'O', 'Z', 'h', 'o', '#', 'M', 'W', '8', 'B', '@', '$'};
 
-            setWindowSize("");
-            char[][] arr = vidToFrames("", shader);
-            playAscii(arr, getFps(""));
+            String vidPath = @"G:\BadApple\BadApple.mp4";
+            IMediaAnalysis vidInfo = FFProbe.Analyse(vidPath);
+
+            setWindowSize(vidInfo);
+            char[][] arr = vidToFrames(vidPath, vidInfo, shader);
+            playAscii(arr, getFps(vidInfo));
         }
 
-        public static void setWindowSize(string vidPath) {
-            int width = 480;  // GET FROM VID
-            int height = 360; // GET FROM VID
+        public static void setWindowSize(IMediaAnalysis vidInfo) {
+            if (vidInfo.PrimaryVideoStream == null) throw new Exception("Primary video stream is null");
+
+            int width = vidInfo.PrimaryVideoStream.Width;
+            int height = vidInfo.PrimaryVideoStream.Height;
             asciiWidth = 37 * (width * 3 / height) * 2;
             asciiHeight = 111;
 
@@ -38,18 +46,25 @@ namespace CMD_Apple
             Console.SetBufferSize(asciiWidth + 8, asciiHeight + 8);
         }
 
-        public static float getFps(string vidPath) {
-            return 30f; // GET FROM VID
+        public static double getFps(IMediaAnalysis vidInfo) {
+            if (vidInfo.PrimaryVideoStream == null) throw new Exception("Primary video stream is null");
+
+            return vidInfo.PrimaryVideoStream.FrameRate;
         }
 
-        public static char[][] vidToFrames(string vidPath, char[] shader) {
+        public static char[][] vidToFrames(string vidPath, IMediaAnalysis vidInfo, char[] shader)
+        {
             // TODO: Set Font Size to 16 (or whatever the default is)
 
-            int frameCount = 6573; // GET FROM VID
+            if (vidInfo.PrimaryVideoStream == null) throw new Exception("Primary video stream is null");
+            int frameCount = (int)Math.Floor(vidInfo.Duration.TotalMilliseconds / 1000 * vidInfo.PrimaryVideoStream.FrameRate) - 1;
+            //frameCount = 1000;
+
             char[][] arr = new char[frameCount][];
             for (int i = 0; i < frameCount; i++)
             {
-                Bitmap img = new Bitmap("F:\\Projects\\CMD_Apple\\res\\frame-" + (i + 1) + ".png"); // GET FROM VID
+                Bitmap img = Snapshot(vidPath, new Size(asciiWidth, asciiHeight), TimeSpan.FromMilliseconds(i * (1000 / vidInfo.PrimaryVideoStream.FrameRate)));
+                //Bitmap img = new Bitmap("F:\\Projects\\CMD_Apple\\res\\frame-" + (i + 1) + ".png"); // GET FROM VID
                 img = new Bitmap(img, new Size(asciiWidth, asciiHeight));
                 arr[i] = new char[(asciiWidth + 1) * asciiHeight];
                 for (int y = 0; y < asciiHeight; y++)
@@ -67,7 +82,7 @@ namespace CMD_Apple
             return arr;
         }
 
-        public static void playAscii(char[][] frames, float fps) {
+        public static void playAscii(char[][] frames, double fps) {
             // TODO: Set Font Size to 8
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
@@ -77,9 +92,25 @@ namespace CMD_Apple
                 Console.Out.WriteAsync(frames[i]);
                 if (watch.Elapsed.TotalMilliseconds < (1 / fps) * 1000 * i)
                 {
-                    Thread.Sleep((int)MathF.Round(((1 / fps) * 1000 * i) - (int)watch.Elapsed.TotalMilliseconds));
+                    Thread.Sleep((int)Math.Round(((1 / fps) * 1000 * i) - (int)watch.Elapsed.TotalMilliseconds));
                 }
             }
+        }
+
+        public static Bitmap Snapshot(string input, Size? size = null, TimeSpan? captureTime = null, int? streamIndex = null, int inputFileIndex = 0)
+        {
+            var source = FFProbe.Analyse(input);
+            var (arguments, outputOptions) = SnapshotArgumentBuilder.BuildSnapshotArguments(input, source, size, captureTime, streamIndex, inputFileIndex);
+            using var ms = new MemoryStream();
+
+            arguments
+                .OutputToPipe(new StreamPipeSink(ms), options => outputOptions(options
+                    .ForceFormat("rawvideo")))
+                .ProcessSynchronously();
+
+            ms.Position = 0;
+            using var bitmap = new Bitmap(ms);
+            return bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), bitmap.PixelFormat);
         }
 
     }
